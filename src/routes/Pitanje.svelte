d47a1d65c7de0b46616d34dc3270ef4f79637c92
mihajlo1923 <pusonjamihajlo@gmail.com>
2024-02-19 12:29:12 +0100

    a

--- src/routes/Pitanje.svelte
+++ src/routes/Pitanje.svelte
@@ -1,11 +1,12 @@
 <script>
+    import { onMount } from 'svelte';
     export let question;
     export let correctAnswer;
     export let incorrectAnswers;
 
     
     let shuffle =(array)=>array.sort(()=> Math.random()-0.5);
-    let posssibleAnswers =shuffle([...incorrectAnswers, correctAnswer]);
+    $:posssibleAnswers =shuffle([...incorrectAnswers, correctAnswer]);
     const handleClick=(da)=>{
         if(da==correctAnswer){
             alert("Tacno")
@@ -15,6 +16,16 @@
         }
 
     }
+    let data;
+    const getQuestion= async()=>{
+        const res=await fetch('https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple')
+        let data=await res.json()
+        console.log(data)
+        question=data.results[0].question;
+        correctAnswer=data.results[0].correct_answer
+        incorrectAnswers=data.results[0].incorrect_answers
+    }
+    onMount(getQuestion)
 </script>
 <div>
     <h3>
